0858ae7b5546f0e01f1f7de903eb129358122a9c
tajima_shun <tajima_shun@plise.co.jp>
2026-06-29 13:42:30 +0900

    Complete Exercise 6: Implement basic views and URL routing. Fixes #2

--- config/urls.py
+++ config/urls.py
@@ -1,24 +1,7 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.backends if hasattr(admin, 'site') else admin.site.urls), # 標準の管理者URL
-    path('', include('apps.bookings.urls')), # bookingsアプリのURLをルートに接続
+    path('admin/', admin.site.urls),         # 標準の管理者URL（ここを修正）
+    path('', include('apps.bookings.urls')),  # bookingsアプリのURLをルートに接続
 ]
